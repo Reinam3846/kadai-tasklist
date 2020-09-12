2413203c3d0c1e0d8d2cee57559207b00cf93e61
reina.miyashiro <reinam3846@gmail.com>
2020-09-12 07:54:38 +0000

    CRUD messages

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -100,7 +100,7 @@ class TasksController extends Controller
         // idの値でメッセージを検索して取得
         $task = Task::findOrFail($id);
         // メッセージを更新
-        $task->content = $task->content;
+        $task->content = $request->content;
         $task->save();
 
         // トップページへリダイレクトさせる
